ed2694ed653f40558f2227aa6aec85e195c4b7ff
Yoram Finder <yoram.finder@gmail.com>
2025-04-06 08:34:53 +0300

    Euler108 optimized

--- euler108/euler108.c
+++ euler108/euler108.c
@@ -5,31 +5,40 @@ Solution: 1/x+1/y=1/n => n*x+n*y=x*y => 0 = n*x+n*y-x*y =>
 n^2 = x*y-n*x-n*y+n^2 => n^2=(x-n)(y-n) => n^2=a*b
 
 If we find n such that total number of all the possible factors for n^2 is greater than
-1,000 we find the solution. This solution is a kind of brute force approach.
+1,000 we find the solution. This solution has optimized factors() function by "jay_kay"
 
 n: 180180
 
-real    2m21.743s
-user    2m21.269s
-sys     0m0.081s
+real    0m0.069s
+user    0m0.061s
+sys     0m0.008s
 
 */
 
 #include <stdio.h>
 
-
-unsigned int factors (unsigned int n) {
-    long unsigned int n2 = (long unsigned int)n*(long unsigned int)n;
-    unsigned int i, c;
-
-    c = 2;
-    for (i = 2; i < n; i++) {
-        if (n2 % i == 0) {
-            c++;
+unsigned int factors (unsigned int number) {
+    unsigned int result = 1;
+    unsigned int exp = 0;
+    while(!(number & 1)) {
+        exp++;
+        number >>= 1;
+    }
+    result *= ((exp << 1) + 1);
+    for(int i = 3; i * i <= number; i += 2) {
+        exp = 0;
+        while(!(number % i)) {
+            exp++;
+            number /= i;
         }
+        result *= ((exp << 1) + 1);
     }
 
-    return c;
+    //  if number > 1 then number is the final prime factor raised to the first power
+    //  since we are calculating the number of factors of number's square
+    //  we multiple by 3 (1 doubled + 1)
+    if(number > 1) result *= 3;
+    return (result + 1) >> 1;
 }
 
 
@@ -40,7 +49,7 @@ int main () {
     n = 1000;
     while(c <= 1000) {
         n++;
-        c = factors(n);
+        c = factors(n); 
     }
 
     printf("n: %u\n", n);
